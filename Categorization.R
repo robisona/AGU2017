69f675258624689a8c8e0171746c91f39665c888
Drew Robison <andrew.l.robison@gmail.com>
2017-11-12 09:12:45 -0500

    Boxplots by Type done

--- Categorization.R
+++ Categorization.R
@@ -20,15 +20,39 @@ NumSite <- length(unique(Data$Site)) # Two of the samples are duplicates
 NumVar <- ncol(Data) # 48 columns
 
 
+
 ##########
-## Categorize data into groups based on land cover ##
+## Start with exmamining the TYPE category, either headwater, tributary, or mainstem/mouth ##
+##########
+unique(Data$Type)
+
+par(mfrow = c(2,2))
+Data$Type <- factor(Data$Type, levels = c("HEAD","TRIB","MAIN"))
+boxplot(Data$CO2_uatm ~ Data$Type, log = "y", ylab = expression('pCO'[2]*' ('*mu*'atm)')) 
+boxplot(Data$CH4_uatm ~ Data$Type, log = "y", ylab = expression('CH'[4]*' ('*mu*'atm)')) 
+boxplot(Data$N2O_uatm ~ Data$Type, log = "y", ylab = expression('N'[2]*'O ('*mu*'atm)')) 
+boxplot(Data$N2.Ar_Diff ~ Data$Type, ylab = expression('N'[2]*':Ar'))
+
+##########
+## Test differences in gas data based on Type ##
 ##########
 
 #Let's first looks at the range of certain land cover categories
 hist(Data$forst_upstream_avg)
 
+# The gas data is not normally distributed, thus we must use non-parametric tests. 
+
+
+
+##########
+## Categorize data into groups based on land cover ##
+##########
+
+
 
 Data$LandUse[Data$allhumn_upstream_avg > 40 & Data$forst_upstream_avg < 30] <- "Urban"
 Data$LandUse[Data$forst_upstream_avg > 40 & Data$allhumn_upstream_avg < 30] <- "Forested"
 Data$LandUse[is.na(Data$LandUse)] <- "Mixed"
+
+
 
